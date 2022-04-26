c7bfd0b4fa7f6fdeb7a067f4080745da50b32edc
Romain <romaingicquiaud@hotmail.fr>
2022-04-26 11:02:14 -0400

    Candlechart with a 'fake' database

---
+++ App/src/app/modules/candlestick/candlestick.component.ts
@@ -0,0 +1,136 @@
+import { Component, ViewChild } from '@angular/core';
+
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexYAxis,
+  ApexXAxis,
+  ApexPlotOptions,
+  ApexDataLabels,
+  ApexStroke
+} from "ng-apexcharts";
+
+import { seriesData, seriesDataLinear } from "./ohlc";
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  yaxis: ApexYAxis;
+  plotOptions: ApexPlotOptions;
+  dataLabels: ApexDataLabels;
+  stroke: ApexStroke;
+};
+
+@Component({
+  selector: 'app-candlestick',
+  templateUrl: './candlestick.component.html',
+  styleUrls: ['./candlestick.component.css']
+})
+export class CandlestickComponent {
+  @ViewChild("chart") chart: ChartComponent | any;
+  public chartCandleOptions: Partial<ChartOptions> | any;
+  public chartBarOptions: Partial<ChartOptions> | any;
+  
+  constructor() {
+    this.chartCandleOptions = {
+      series: [
+        {
+          name: "candle",
+          data: seriesData
+        }
+      ],
+      chart: {
+        type: "candlestick",
+        height: 290,
+        id: "candles",
+        toolbar: {
+          autoSelected: "pan",
+          show: false
+        },
+        zoom: {
+          enabled: false
+        }
+      },
+      plotOptions: {
+        candlestick: {
+          colors: {
+            upward: "#3C90EB",
+            downward: "#DF7D46"
+          }
+        }
+      },
+      xaxis: {
+        type: "datetime"
+      }
+    };
+
+    this.chartBarOptions = {
+      series: [
+        {
+          name: "volume",
+          data: seriesDataLinear
+        }
+      ],
+      chart: {
+        height: 160,
+        type: "bar",
+        brush: {
+          enabled: true,
+          target: "candles"
+        },
+        selection: {
+          enabled: true,
+          xaxis: {
+            min: new Date("20 Jan 2017").getTime(),
+            max: new Date("10 Dec 2017").getTime()
+          },
+          fill: {
+            color: "#ccc",
+            opacity: 0.4
+          },
+          stroke: {
+            color: "#0D47A1"
+          }
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      plotOptions: {
+        bar: {
+          columnWidth: "80%",
+          colors: {
+            ranges: [
+              {
+                from: -1000,
+                to: 0,
+                color: "#F15B46"
+              },
+              {
+                from: 1,
+                to: 10000,
+                color: "#FEB019"
+              }
+            ]
+          }
+        }
+      },
+      stroke: {
+        width: 0
+      },
+      xaxis: {
+        type: "datetime",
+        axisBorder: {
+          offsetX: 13
+        }
+      },
+      yaxis: {
+        labels: {
+          show: false
+        }
+      }
+    };
+  }
+}
